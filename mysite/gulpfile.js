2b061347ae2926d641073cab984f6b9db7c4aaa9
Ernest C. Davis <Ernest C. Davis>
2017-07-27 15:12:28 -0400

    Base React Framework established to produce blank page.

---
+++ mysite/gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// generate_dependencies
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+
+// SCSS/CSS
+
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
+
+// Compile SCSS to CSS
+gulp.task('compile_scss', function(){ 
+	gulp.src(SCSS_SRC)
+	.pipe(sass().on('errpr', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(rename({suffix: '.min'}))	
+	.pipe(changed(SCSS_DEST))	
+	.pipe(gulp.dest(SCSS_DEST));
+
+});
+
+// detect changes in SCSS and compile to CSS
+gulp.task('watch_scss', function(){
+	gulp.watch(SCSS_SRC, ['compile_scss']);
+
+});
+
+
+// Run tasks
+
+gulp.task('default', ['watch_scss']);
